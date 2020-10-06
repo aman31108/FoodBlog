46b989ea6acc0ad4a9bdc6642e5a080d8a9362b8
aman31108 <aman31108@users.noreply.github.com>
2020-10-06 22:04:52 +0530

    Updates

---
+++ precache-manifest.015a6462b247fa0a409b36f444d70443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7cf35e7bd4d1508ae728f8f89e1b3a",
+    "url": "/FoodBlog/index.html"
+  },
+  {
+    "revision": "3e9b707125278dfeb7e0",
+    "url": "/FoodBlog/static/css/main.d97241ae.chunk.css"
+  },
+  {
+    "revision": "ee574d810fb8a3a165f2",
+    "url": "/FoodBlog/static/js/2.890f7149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FoodBlog/static/js/2.890f7149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9b707125278dfeb7e0",
+    "url": "/FoodBlog/static/js/main.25a614cf.chunk.js"
+  },
+  {
+    "revision": "08aaa316a036626a7b63",
+    "url": "/FoodBlog/static/js/runtime-main.14d01e51.js"
+  },
+  {
+    "revision": "e02a03935a387cdd2116b4e70731b954",
+    "url": "/FoodBlog/static/media/bg.e02a0393.png"
+  }
+]);
