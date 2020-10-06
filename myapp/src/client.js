9b0bdc940c088005a80d2498d9b90a6e490c202e
aman31108 <aman31108@users.noreply.github.com>
2020-10-06 20:40:22 +0530

    my first commit

---
+++ myapp/src/client.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful'
+
+export const client = contentful.createClient({
+  space: "cnvgecwuyepg",
+   accessToken: "BRhFU15DXmzVXkGs4iBFuejigajvw-SNLE7yuPIVgMU"
+})
